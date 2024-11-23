fc63b25972379e19d51838ab681505a1c67bbcef
firdaus <firdausmila664@gmail.com>
2024-11-23 12:11:03 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Linktree </title>
+    <link rel="stylesheet" href="style.css">
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <img src="ft.jpg" alt="Logo" class="logo">
+            <h1>Muhamad Firdaus</h1>
+            <p>Web Developer | Freelancer</p>
+        </div>
+
+        <div class="links">
+            <a href="https://wa.me/+6285924518195" target="_blank" class="link whatsapp">
+                <i class="fab fa-whatsapp"></i> WhatsApp
+            </a>
+            <a href="https://www.instagram.com/ih.dausa/" target="_blank" class="link instagram">
+                <i class="fab fa-instagram"></i> Instagram
+            </a>
+            <a href="https://www.facebook.com/daus.azhaa/" target="_blank" class="link facebook">
+                <i class="fab fa-facebook"></i> Facebook
+            </a>
+            <a href="https://github.com/dausfla" target="_blank" class="link github">
+                <i class="fab fa-github"></i> GitHub
+            </a>
+        </div>
+
+        <footer>
+            <p>&copy; 2024 Muhamad Firdaus. Semua hak dilindungi.</p>
+        </footer>
+    </div>
+</body>
+</html>
